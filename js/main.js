961e8cd162ce3784fae60a00fee6f0c6f4aed5ae
Kanishk Tiwari <42186454+kt26@users.noreply.github.com>
2020-04-17 06:03:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,21 @@
     
     /*==================================================================
     [ Validate ]*/
+    
+    
+    
+	var input = document.getElementById( 'file-upload' );
+    var infoArea = document.getElementById( 'file-upload-filename' );
+input.addEventListener( 'change', showFileName );
+function showFileName( event ) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = fileName;
+}
+	
+    
+    
+    
     var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
